64242907295beb4d4367edbd9f80dcc37627a2b2
Crystal Atkinson <crystaltayloratkinson@gmail.com>
2021-03-05 13:59:13 -0500

    Hard coded

--- src/components/GetImage.jsx
+++ src/components/GetImage.jsx
@@ -2,33 +2,33 @@ import React, { Component } from 'react';
 
 
 class GetImage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            gif: "",
-            title: ""
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+        gif: "",
+        title: ""
     }
+  }
 
-    async componentDidMount () {
-        const url = `https://api.giphy.com/v1/gifs/random?api_key=${APIKEY}&tag=cat`;
-        const gifData = await fetch(url)
-          .then(response => response.json());
-        this.setState({
-          gif: gifData.data.image_url,
-          title: gifData.data.title
-        });
-        console.log('THIS IS THE GIF: ', this.state.gif)
-      }
+  async componentDidMount () {
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=vKQFVBkKu37h3XrK4p8e4J4AZGtI0NMX&tag=cat`;
+    const gifData = await fetch(url)
+      .then(response => response.json());
+    this.setState({
+      gif: gifData.data.image_url,
+      title: gifData.data.title
+    });
+    console.log('THIS IS THE GIF: ', this.state.gif)
+  }
 
-    render() {
+  render() {
 
-        return (
-            <>
-                <img src={this.state.gif} alt={this.state.title}/>
-            </>
+    return (
+      <>
+        <img src={this.state.gif} alt={this.state.title}/>
+      </>
     )
-    }
+  }
     
 }
 export default GetImage;
